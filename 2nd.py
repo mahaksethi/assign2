2bb95de7585b6e9c0f7122af72683ea1715aa181
mahak sethi <mahaksethi05@gmail.com>
2018-06-06 08:33:45 -0700

    (done 2nd assignment successfully)

---
+++ 2nd.py
@@ -0,0 +1,34 @@
+#ques1
+print("arise awake and stop not till the goal is acheived")
+
+#ques2
+print("acad"+"view")
+
+#ques3
+x="manuveer"
+y=15
+z=40
+print(x,y,z)
+#another way
+print(x)
+print(y)
+print(z)
+
+#question4ss
+print('let\'s get started')
+
+#question5
+s="acadview"
+course="python"
+fees=5000
+print("Institue name is %s,course is %s,fees is %d"%(s,course,fees))
+
+#question6
+name="Tony Stark"
+salary=1000000
+print('%s %d'%(name,salary))
+
+
+
+
+
